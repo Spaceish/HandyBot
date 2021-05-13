e7eede9194eb3409dd8806077bf3fd946b1c8ea1
ZenoMilk12 <xalexclashroyale@gmail.com>
2021-05-13 12:00:55 +0300

    adding cat facts

---
+++ cogs/cat.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import random
+import requests
+
+class Cat(commands.Cog):
+
+    def __init__(self, handy):
+        self.handy = handy
+        ids = []
+
+    @commands.command()
+    async def cfacts(self, ctx):
+        await ctx.send(f'**Fact :** catfacts()')
+
+def setup(handy):
+    handy.add_cog(Cat(handy))
+
+def catfacts():
+    ids = ["58e008800aac31001185ed07", "58e008630aac31001185ed01", "58e00a090aac31001185ed16", "58e009390aac31001185ed10", "58e008780aac31001185ed05"]
+    id = random.choice(ids)
+    fact = requests.get(f'https://cat-fact.herokuapp.com/facts/{id}')
+
+    return fact.json()['text']
