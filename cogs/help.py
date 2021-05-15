e3266c2251aa3ffd241622ba05c4f024264c5394
ZenoMilk12 <xalexclashroyale@gmail.com>
2021-05-15 21:44:09 +0300

    finished the help command yaya

--- cogs/help.py
+++ cogs/help.py
@@ -2,15 +2,52 @@ import discord
 from discord.ext import commands
 
 class Help(commands.Cog):
-    def __init__(self, ctx):
+
+    def __init__(self, handy):
+        self.handy = handy
+
+    @commands.command()
+    async def help(self, ctx):
+        embed = discord.Embed(
+            colour = discord.Colour.blurple()
+        )
+
+        embed.set_author(name="Help")
+        help_command = {
+            "help" : "Shows this message"
+        }
 
         cat = {
-            commands : ["cfacts"]
+            "cfacts" : "Facts about cats."
         }
 
         math = {
-            commands : ["sum", ""]
+            "sum" : "Sum of two numbers",
+            "min" : "Difference between two numbers",
+            "mul" : "Multiply two numbers",
+            "div" : "Divide two numbers"
         }
+
+
+        music = {
+            "play" : "Play music from a url or keyword",
+            "join" : "Join a channel",
+            "leave" : "Leave the channel",
+            "stop" : "Stops the music",
+            "pause" : "Pauses the music",
+            "resume" : "Resumes the music"
+        }
+
+        def render(commands_dict):
+            for command in list(commands_dict):
+                embed.add_field(name=command, value=commands_dict.pop(command), inline=False)
+
+        render(help_command)
+        render(cat)
+        render(math)
+        render(music)
+
+        await ctx.send(embed=embed)
 
 
 def setup(handy):
