35773a7cc5da4b578e9d02919a69042e22fad19b
ZenoMilk12 <xalexclashroyale@gmail.com>
2021-05-16 13:31:49 +0100

    fixing typo lol

--- cogs/math.py
+++ cogs/math.py
@@ -30,7 +30,7 @@ class Math(commands.Cog):
             await ctx.send(f'**Number : {a}**\n**Exception : Cannot get the square root of negative numbers.(Bot cannot handle complex math yet so, this is limited to basic math.)**')
     @commands.command()
     async def pow(self, ctx, a : float, power : float):
-        await ctx.send(f'**Number : {a}**\n**Power : {b}**\n**The result : {pow(a, power)}**')
+        await ctx.send(f'**Number : {a}**\n**Power : {power}**\n**The result : {pow(a, power)}**')
 
 
 def setup(handy):
